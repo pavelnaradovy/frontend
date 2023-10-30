cbf11d70abd3f5026c6681b1985068328655344d
Pavel Naradovy <pavel.narodovy@beanmachine.dev>
2023-10-30 16:31:04 +0200

    add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: ["airbnb-base"],
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  rules: {
+    // Add project-specific rules or overrides here
+  },
+};
