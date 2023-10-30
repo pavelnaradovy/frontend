279803abea3f59aede88ed1efba4f1127db497a5
Pavel Naradovy <pavel.narodovy@beanmachine.dev>
2023-10-30 18:10:22 +0200

    fx navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,4 @@
-import styles from "./styles.css";
+import styles from "./styles.module.css";
 export default function Navbar() {
-  return <div className={styles.navBar}></div>;
+  return <div className={styles.navBar}><h1>dasdasdsadsadas</h1></div>;
 }
