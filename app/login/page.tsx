45979eae1a4b5e96a28af1e9cc6aa9489e77c49c
Pavel Naradovy <pavel.narodovy@beanmachine.dev>
2023-11-02 14:29:09 +0200

    add sign-in page

---
+++ app/login/page.tsx
@@ -0,0 +1,8 @@
+import styles from "./styles.module.sass";
+export default function Login() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}></div>
+    </div>
+  );
+}
