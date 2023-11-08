d20109ebdf62b4ac9459a3530fb5407baca42d90
Pavel Naradovy <pavel.narodovy@beanmachine.dev>
2023-11-08 20:15:05 +0200

    Configure redux

--- app/page2/page.tsx
+++ app/page2/page.tsx
@@ -1,45 +1,21 @@
-import Image from 'next/image'
-import styles from '../page.module.css'
+"use client";
+
+import Image from "next/image";
+import styles from "../page.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, useAppSelector } from "@/redux/store";
+import { login } from "@/redux/features/auth-slice";
 
 export default function Home() {
+  const dispatch = useDispatch<AppDispatch>();
+  const loginFunc = () => {
+    dispatch(login("lolka"));
+  };
+  const username = useAppSelector((state) => state.authReducer.value.username);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-   
-    </main>
-  )
+    <div>
+      <button onClick={loginFunc}>Lolka polka</button>
+      <span>1231232{username}</span>
+    </div>
+  );
 }
---
+++ redux/features/auth-slice.ts
@@ -0,0 +1,34 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+
+type InitialState = {
+    value: AuthState;
+
+}
+type AuthState = {
+    isAuth: boolean,
+    username: string,
+    isModerator: boolean
+}
+const initialState = {
+    value: {
+        isAuth: false,
+        username: "",
+        isModerator: false
+    } as AuthState
+} as InitialState
+export const auth = createSlice({
+    name: "auth", initialState, reducers: {
+        logOut: () => { return initialState },
+        login: (state, action: PayloadAction<string>) => {
+            return {
+                value: {
+                    isAuth: true, username: action.payload, isModerator: false
+                }
+            }
+        }
+    }
+})
+
+export const { login, logOut } = auth.actions
+export default auth.reducer
